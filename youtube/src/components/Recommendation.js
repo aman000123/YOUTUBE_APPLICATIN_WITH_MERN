3514fa03e9e149fd66a2dc6954e370b40f76b7d1
aman <amishra73185@gmail.com>
2023-10-13 11:27:34 +0530

    first commit

---
+++ youtube/src/components/Recommendation.js
@@ -0,0 +1,44 @@
+
+
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Card from "./Card";
+import styled from "styled-components";
+
+
+const Container = styled.div`
+  flex: 2;
+
+  @media (min-width:760px)and (max-width:1024px){
+
+    flex: 2;
+    display: flex;
+    justify-content: space-between;
+    
+
+
+}
+
+`;
+
+
+const Recommendation = ({ tags }) => {
+    const [videos, setVideos] = useState([]);
+
+    useEffect(() => {
+        const fetchVideos = async () => {
+            const res = await axios.get(`http://localhost:4005/api/videos/tags?tags=${tags}`);
+            setVideos(res.data);
+        };
+        fetchVideos();
+    }, [tags]);
+
+    return (
+        <Container>
+            {videos.map((video) => (
+                <Card type="sm" key={video._id} video={video} />
+            ))}
+        </Container>
+    );
+};
+export default Recommendation
