0566c7701ccd529fd078b5c72d7625259c2256b1
Aman mishra <124899503+aman000123@users.noreply.github.com>
2023-10-13 12:37:36 +0530

    Update Card.js

--- youtube/src/components/Card.js
+++ youtube/src/components/Card.js
@@ -16,7 +16,7 @@ const Card = ({ type, video }) => {
   useEffect(() => {
     const fetchChannel = async () => {
       try {
-        const res = await axios.get(`http://localhost:4005/api/users/find/${video.userId}`);
+        const res = await axios.get(`https://youtubes-app-1ktq.onrender.com/api/users/find/${video.userId}`);
         setChannel(res.data);
       } catch (error) {
         console.error("Error fetching channel:", error);
