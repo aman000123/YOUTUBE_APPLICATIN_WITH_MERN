b28c1108e40195b6dd7dd248a23ce6b2ad00913d
aman <amishra73185@gmail.com>
2023-10-21 12:47:07 +0530

    updates

--- YT-Server/Controllers/auth.js
+++ YT-Server/Controllers/auth.js
@@ -30,7 +30,7 @@ const signup = async (req, res, next) => {
         res.status(200).send("User has been created");
     } catch (err) {
         //  res.status(500).json({ message: 'An error occurred while signup in', error: err });
-        res.status(500).json({ error: err });
+        res.status(500).send(err);
 
     }
 }
