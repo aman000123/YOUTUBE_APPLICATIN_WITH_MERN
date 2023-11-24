d4b8ad89cf87290f46b2b23c86037a0e99db3c40
Aman  mishra <124899503+aman000123@users.noreply.github.com>
2023-11-24 15:18:15 +0530

    Update app.js

--- YT-Server/app.js
+++ YT-Server/app.js
@@ -47,7 +47,7 @@ const corsOptions = {
 
     // origin: "https://6536086434f65672ef06dab4--hilarious-rugelach-8effd5.netlify.app",
 
-    origin: "https://65606db67dfa152ccd9daa9e--effulgent-platypus-3d2956.netlify.app",
+    origin: "https://65607147ef3d102529c3b542--zesty-dodol-818e9e.netlify.app",
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // Enable cookies and authentication headers
 };
